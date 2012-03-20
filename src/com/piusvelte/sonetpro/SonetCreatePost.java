07f9f39e60af03ae262c6a22b33c037e2ac8e938
piusvelte <piusvelte@gmail.com>
2012-03-20 13:28:53 -0400

    remove unneeded imports

--- src/com/piusvelte/sonetpro/SonetCreatePost.java
+++ src/com/piusvelte/sonetpro/SonetCreatePost.java
@@ -19,7 +19,6 @@
  */
 package com.piusvelte.sonetpro;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -35,11 +34,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ContentBody;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
